fbc61298b955183b6ea59af36eeaeb50442880db
RAJUKUMAR S <97968060+SRK-RAJU@users.noreply.github.com>
2022-02-18 22:55:40 +0530

    updating

--- terraform/roboshop/main.tf
+++ terraform/roboshop/main.tf
@@ -33,7 +33,7 @@ resource "null_resource" "ansible" {
   provisioner "remote-exec" {
     connection {
       host     = element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)
-      user     = "root"
+      user     = "centos"
       password = "DevOps321"
     }
     inline = [
